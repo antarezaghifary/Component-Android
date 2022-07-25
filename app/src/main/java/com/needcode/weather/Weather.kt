044c81abfc21dbfbafd6a3f60829f1bce8ad37cf
azerid <antareza08@gmail.com>
2022-07-25 15:41:05 +0700

    add class name, add fonts, add colors, add dimens, add themes

--- app/src/main/java/com/needcode/weather/Weather.kt
+++ app/src/main/java/com/needcode/weather/Weather.kt
@@ -1,7 +1,16 @@
 package com.needcode.weather
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import timber.log.Timber
 
 class Weather:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        Timber.e("run base application")
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 
 }
