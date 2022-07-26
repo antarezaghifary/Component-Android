7b6ddd284ff5a9a7f81315eaef34685c7cc55483
azerid <antareza08@gmail.com>
2022-07-26 13:18:54 +0700

    add chip static

---
+++ app/src/main/java/com/needcode/weather/chip/ChipActivity.kt
@@ -0,0 +1,21 @@
+package com.needcode.weather.chip
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.needcode.weather.databinding.ActivityChipBinding
+
+class ChipActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityChipBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        with(binding) {
+
+        }
+    }
+}
